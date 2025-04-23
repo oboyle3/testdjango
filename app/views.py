bf3522c68d7ea87f72f0c935c71ec5f830170c87
oboyle3 <po720999@sju.edu>
2025-04-22 21:24:08 -0400

    another commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Student
+# Create your views here.
+def index(request):
+    obj=Student.objects.all()
+    context={
+        "obj": obj,
+    }
+    return render(request, "index.html", context)
