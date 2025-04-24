e33214883d15f2bd7ff0c5d9af4a7c6b8b0cf03b
oboyle3 <po720999@sju.edu>
2025-04-23 22:39:01 -0400

    added another model to link teachers and courses

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Student
+from .models import Course
+from .models import Teacher
 
 admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Teacher)
--- app/models.py
+++ app/models.py
@@ -1,9 +1,47 @@
 from django.db import models
 
-# Create your models here.
-class Student (models.Model):
+# 3. lets create a model that represents a teacher
+class Teacher(models.Model):
+    # name of the teacher
     name = models.CharField(max_length=100)
-    des=models.TextField()
+
+    # subject they teach
+    subject = models.CharField(max_length=100)
+
+    # this makes it easier to see the course name in django admin or shell
     def __str__(self):
         return self.name
-        
+
+#1. Lets create a model that reprents a course that students can be enrolled in
+class Course(models.Model):
+    # name of the course e.g. math 101
+    title = models.CharField(max_length=100)
+
+    # optional description of the course
+    description = models.TextField(blank=True)
+
+    # links to step 3 lets connect a course to a teacher
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True)
+
+    # this makes it easier to see the course name in django admin or shell
+    def __str__(self):
+        return self.title
+    
+# 2. Lets create a model that represents a student
+class Student(models.Model):
+    # Name of the student
+    name = models.CharField(max_length=100)
+
+    # description
+    des = models.TextField()
+
+    # ForeignKey creates a many to one relationship to course
+    # this means many students can be enrolled in one course
+    #this connects each student to one course, if a course is deleted any students in that course are deleted
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+
+     # this makes it easier to see the course name in django admin or shell
+    def __str__(self):
+        return self.name
+    
+
